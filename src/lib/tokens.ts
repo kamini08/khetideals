2fad60942d5d0f8171e74e8e62d0df55e31bed30
Prajwal-kp-18 <prajwal.kp.1817@gamil.com>
2024-08-29 00:14:35 +0530

    2FA small change

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -56,7 +56,7 @@ export const generatePasswordResetToken = async (email: string) => {
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
 
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const existingToken = await getVerificationTokenByEmail(email);
 
