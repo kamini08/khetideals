e84c82d1825367f27c1597fa382c096416fa977f
BhoomiAgrawal12 <bhoomiagrawal1212@gmail.com>
2024-09-09 16:26:04 +0530

    form.css added

---
+++ src/app/api/myland/route.ts
@@ -0,0 +1,34 @@
+import clientPromise from "@/lib/mongodb";
+import LandholderModel from "@/models/landHolder"; // Replace with your model
+import { NextResponse } from "next/server";
+import { auth } from "../../../../auth";
+
+export async function GET(req: Request) {
+    const session = await auth();
+    const mainId = session?.user.id;
+
+try {
+    // Connect to MongoDB
+    await clientPromise();
+
+    // Query the database to find all entries with the matchi const document = await SharecropperModel.findOne({ mainId: mainId });
+    const document = await LandholderModel.find({ mainId: mainId });
+
+
+    if (!document) {
+        return new Response(
+            JSON.stringify({
+                message: "Document with the specified mainId not found",
+            }),
+            { status: 404 }
+        );
+    }
+    // Return the data as JSON
+    return new Response(JSON.stringify(document), { status: 200 });
+} catch (error: any) {
+    console.error("Error fetching document:", error);
+    return new Response(JSON.stringify({ message: error.message }), {
+        status: 400,
+    });
+}
+}
